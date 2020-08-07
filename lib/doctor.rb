e5135bf128692809020e16959a1454cfeda775da
Jacob Viti <thevitij64@gmail.com>
2020-08-07 02:02:00 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,3 +1,16 @@
 class Doctor
+  attr_accessor :name, :appointment, :doctor
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 
 end
